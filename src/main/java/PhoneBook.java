d19b4c25c110568448fdb6f10ec8661e00273a75
spMakumazan <sp.makumazan@gmail.com>
2023-05-14 12:11:40 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    private Map<String, String> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+
+    public int add(String name, String phone) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        int excepted = 3;
+
+        phoneBook.add("Игорь", "89909909090");
+        phoneBook.add("Сергей", "89909919191");
+        phoneBook.add("Анна", "89909929292");
+        int result = phoneBook.add("Игорь", "89909949494");
+
+        Assertions.assertEquals(excepted, result);
+    }
 }
