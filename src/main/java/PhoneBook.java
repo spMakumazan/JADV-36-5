6408f326c88fab5e467fee7b2a9eb6bf7ba31dd6
spMakumazan <sp.makumazan@gmail.com>
2023-05-14 12:35:29 +0300

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -15,4 +15,8 @@ public class PhoneBook {
         }
         return phoneBook.size();
     }
+
+    public String findByNumber(String phone) {
+        return null;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -3,15 +3,25 @@ import org.junit.jupiter.api.Test;
 
 public class PhoneBookTest {
 
+    PhoneBook phoneBook = new PhoneBook();
     @Test
     public void addTest() {
-        PhoneBook phoneBook = new PhoneBook();
+
         int excepted = 3;
 
         phoneBook.add("Игорь", "89909909090");
         phoneBook.add("Сергей", "89909919191");
         phoneBook.add("Анна", "89909929292");
         int result = phoneBook.add("Игорь", "89909949494");
+
+        Assertions.assertEquals(excepted, result);
+    }
+
+    @Test
+    public void findByNumbeTest() {
+        String excepted = "Игорь";
+
+        String result = phoneBook.findByNumber("89909909090");
 
         Assertions.assertEquals(excepted, result);
     }
